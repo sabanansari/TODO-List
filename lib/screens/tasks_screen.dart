20da1e88b9b9f49ecf47429398f71e1c2e34eb9a
sabanansari <alisaban52@gmail.com>
2020-07-20 05:49:39 +0530

    listview added

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,40 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              child:
-                  Icon(Icons.list, size: 30.0, color: Colors.lightBlueAccent),
-              backgroundColor: Colors.white,
-              radius: 30.0,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(Icons.list,
+                      size: 30.0, color: Colors.lightBlueAccent),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text('TODO',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.w700,
+                    )),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('TODO',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700,
-                )),
-            Text(
-              '12 Tasks',
-              style: TextStyle(
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
                 color: Colors.white,
-                fontSize: 18.0,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
               ),
-            )
-          ],
-        ),
+              child: TasksList(),
+            ),
+          ),
+        ],
       ),
     );
   }
